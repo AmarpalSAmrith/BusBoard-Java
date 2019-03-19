da5a53bd559cdcf4676953fd8a3c3b6984cfbedf
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-19 11:48:41 +0000

    Reading API with the Jackson 5

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,20 @@
 package training.busboard;
 
+import apple.laf.JRSUIConstants;
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        List<ArrivalPrediction> name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(new GenericType<List<ArrivalPrediction>>() {});
+        System.out.println(name);
     }
 }	
